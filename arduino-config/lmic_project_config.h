39a689a16e18ca3aaded6a299ff343b8a43550bd
Clifford Karani <koomekoome902@gmail.com>
2025-06-24 20:18:35 +0300

    Add LMIC project configuration for EU868 band and SX1276 radio

---
+++ arduino-config/lmic_project_config.h
@@ -0,0 +1,6 @@
+//lmic project config
+#ifndef _lmic_project_config_h_
+#define _lmic_project_config_h_
+#define CFG_eu868 1  // EU 863-870 MHz band (Common for Europe, Africa, parts of Asia)
+#define CFG_sx1276_radio 1
+#endif  // _lmic_project_config_h_
